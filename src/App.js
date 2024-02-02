e85bb2f35e3867bab9b48841746a8f83c7d4c74f
esther-v <esvalas@gmail.com>
2024-02-02 16:58:02 +0100

    add navbar

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import { Switch, Route, Link } from 'react-router-dom'
+import { Layout, Typography, Space } from 'antd';
+import './App.css';
+
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className='navbar'>
+        <Navbar />
+      </div>
+      <div className='main'>
+
+      </div>
+      <div className='footer'>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
